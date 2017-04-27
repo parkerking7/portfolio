098467650636ab0006c4d6371baa4f04c92eac21
Parker King <parker.king0807@gmail.com>
2017-04-27 11:24:09 -0600

    Putting portfolio in its own repository

---
+++ app.js
@@ -0,0 +1,30 @@
+var app = angular.module("portApp", ["ngRoute"]);
+
+app.config(["$routeProvider", function ($routeProvider) {
+
+	$routeProvider
+		.when("/enter", {
+			templateUrl: "enter/enter.html",
+            controller:"EnterController"
+		})
+		.when("/home", {
+			templateUrl: "home/home.html",
+			controller: "HomeController"
+		})
+		.when("/about", {
+			templateUrl: "about/about.html",
+			controller: "AboutController"
+		})
+		.when("/works", {
+			templateUrl: "works/works.html",
+			controller: "WorksController"
+		})
+		 .otherwise({
+		 	redirectTo: "/enter"
+		 });
+//        .otherwise({
+//            redirectTo: "/home"
+//        });
+}]);
+
+
